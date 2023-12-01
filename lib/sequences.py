b6b6d402d7de4b2dd059be500e8bd04ba3cd8b8a
Gathukumi <gathukumidavid001@gmail.com>
2023-12-01 19:10:49 +0300

    completd lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a, b = 0, 1
+
+    fibonacci_sequence = []
+
+    for i in range(length):
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+
+    print(fibonacci_sequence)    
+
